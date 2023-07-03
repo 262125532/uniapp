fcd642198961e3fb2b13843f8b5d86b66c84e096
weijianyi <346735572@qq.com>
2023-07-03 15:21:27 +0800

    完成:车辆-->工时详情更多静态页面开发

--- common/platform.js
+++ common/platform.js
@@ -1,93 +1,94 @@
 //定义基本的urlconst baseUrl= 'http://www.lhdtest.com';/**使用条件编译获取平台信息*/
-let baseUrl= '';
+let baseUrl = '';
 // 线上：iot-api.ubsense.cn
 // 测试：ai-api-test.sany.com.cn
 // 测试环境 development
-if(process.env.NODE_ENV == "development"){
-	baseUrl= 'https://iot-api.ubsense.cn';
-	// baseUrl= 'https://ai-api-test.sany.com.cn';
+if (process.env.NODE_ENV == "development") {
+	// baseUrl= 'https://iot-api.ubsense.cn';
+	baseUrl = 'https://ai-api-test.sany.com.cn';
 }
 // 生产环境 production
-else{
-	baseUrl= 'https://iot-api.ubsense.cn';
+else {
+	baseUrl = 'https://iot-api.ubsense.cn';
 }
+
 function ifDefPlatform() {
 	let platform = ""
 	//App
 	//#ifdef APP-PLUS
 	platform = 'APP-PLUS'
 	//#endif
-	
+
 	//App nvue
 	//#ifdef APP-PLUS-NVUE
 	platform = 'APP-PLUS-NVUE'
 	//#endif
-	
+
 	//H5
 	//#ifdef H5
 	platform = 'H5'
 	//#endif
-	
+
 	//微信小程序
 	//#ifdef MP-WEIXIN
 	platform = 'MP-WEIXIN'
 	//#endif
-	
+
 	//支付宝小程序
 	//#ifdef MP-ALIPAY
 	platform = 'MP-ALIPAY'
 	//#endif
-	
+
 	//百度小程序
 	//#ifdef MP-BAIDU
 	platform = 'MP-BAIDU'
 	//#endif
-	
+
 	//字节跳动小程序
 	//#ifdef MP-TOUTIAO
 	platform = 'MP-TOUTIAO'
 	//#endif
-	
+
 	//飞书小程序
 	//#ifdef MP-LARK
 	platform = 'MP-LARK'
 	//#endif
-	
+
 	//QQ小程序
 	//#ifdef MP-QQ
 	platform = 'MP-QQ'
 	//#endif
-	
+
 	//快手小程序
 	//#ifdef MP-KUAISHOU
 	platform = 'MP-KUAISHOU'
 	//#endif
-	
+
 	//360小程序
 	//#ifdef MP-360
 	platform = 'MP-360'
 	//#endif
-	
+
 	//微信小程序/支付宝小程序/百度小程序/字节跳动小程序/飞书小程序/QQ小程序/快手小程序/360小程序
 	// //#ifdef MP
 	// platform = 'MP'
 	// //#endif
-	
+
 	//快应用通用(包含联盟、华为)
 	//#ifdef QUICKAPP-WEBVIEW
 	platform = 'QUICKAPP-WEBVIEW'
 	//#endif
-	
+
 	//快应用联盟
 	//#ifdef QUICKAPP-WEBVIEW-UNION
 	platform = 'QUICKAPP-WEBVIEW-UNION'
 	//#endif
-	
+
 	//快应用华为
 	//#ifdef QUICKAPP-WEBVIEW-HUAWEI
 	platform = 'QUICKAPP-WEBVIEW-HUAWEI'
 	//#endif
-	
+
 	return platform
 }
 //通过env和平台得到最终的url,因为h5开发时要配置跨域
---
+++ pages/cars/workingHourMore.vue
@@ -0,0 +1,365 @@
+<template>
+	<view class="content">
+		<navBar :navBar="navBar" />
+
+		<view class="date-select">
+			2022年11月19日
+		</view>
+		<view class="title">
+			统计指标
+		</view>
+		<view class="card-box">
+			<view class="uni-flex uni-row">
+				<view class="item" style="background-color: rgba(51,112,255,0.08);">
+					<view class="t">
+						平均工作时长
+					</view>
+					<view class="value" style="color: #3370FF;">
+						999<text class="bit">小时</text>
+					</view>
+				</view>
+				<view class="item" style="background-color: rgba(82,196,26,0.08);">
+					<view class="t">
+						平均运行时长
+					</view>
+					<view class="value" style="color: #52C41A;">
+						999<text class="bit">小时</text>
+					</view>
+				</view>
+				<view class="item" style="background-color: rgba(250,100,0,0.08)">
+					<view class="t">
+						平均怠速时长
+					</view>
+					<view class="value" style="color: #FA6400;">
+						999<text class="bit">小时</text>
+					</view>
+				</view>
+			</view>
+
+			<view class="uni-flex uni-row">
+				<view class="text" style="-webkit-flex: 1;flex: 1;">
+					<view class="uni-flex uni-row item" style="background-color: #F4F7FA">
+						<view class="left-icon">
+							<image class="img" src="../../static/img/累计工作.png"></image>
+						</view>
+						<view class="">
+							<view style="background-color: #F4F7FA;">
+								<view class="t">
+									平均利用率
+								</view>
+								<view class="value" style="color: #000;">
+									999<text class="bit">%</text>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+
+				<view class="text" style="-webkit-flex: 1;flex: 1;">
+					<view class="uni-flex uni-row item" style="background-color: #F4F7FA">
+						<view class="left-icon">
+							<image class="img" src="../../static/img/累计出勤.png"></image>
+						</view>
+						<view class="">
+							<view style="background-color: #F4F7FA;">
+								<view class="t">
+									出勤天数
+								</view>
+								<view class="value" style="color: #000;">
+									999<text class="bit">天</text>
+								</view>
+							</view>
+						</view>
+
+					</view>
+
+				</view>
+			</view>
+		</view>
+
+
+
+
+		<view class="title">
+			每日详情
+		</view>
+
+		<view class="card-box">
+			<view class="day-row">
+				<view class="date">
+					12-01
+				</view>
+				<view class="uni-flex uni-row day-detail">
+					<view class="value">
+						<image class="img" src="../../static/img/持续运行.png"></image>
+						<text>运行时长:</text>
+						<text class="val" style="color: #52C41A;">7小时1分</text>
+					</view>
+					<view class="value">
+						<image class="img" src="../../static/img/持续运行.png"></image>
+						<text>运行时长:</text>
+						<text class="val" style="color: #FF6000;">7小时1分</text>
+					</view>
+				</view>
+
+			</view>
+			<view class="day-row">
+				<view class="date">
+					12-01
+				</view>
+				<view class="uni-flex uni-row day-detail">
+					<view class="value">
+						<image class="img" src="../../static/img/持续运行.png"></image>
+						<text>运行时长:</text>
+						<text class="val" style="color: #52C41A;">7小时1分</text>
+					</view>
+					<view class="value">
+						<image class="img" src="../../static/img/持续运行.png"></image>
+						<text>运行时长:</text>
+						<text class="val" style="color: #FF6000;">7小时1分</text>
+					</view>
+				</view>
+
+			</view>
+
+
+		</view>
+
+
+		<!-- <view class="uni-flex uni-row">
+			<view class="text" style="width: 200rpx;">固定宽度</view>
+			<view class="text" style="-webkit-flex: 1;flex: 1;">自动占满</view>
+			<view class="text" style="width: 200rpx;">固定宽度</view>
+		</view>
+		<view class="uni-flex uni-row" style="-webkit-flex-wrap: wrap;flex-wrap: wrap;">
+			<view class="text" style="width: 280rpx;">一行显示不全,wrap折行</view>
+			<view class="text" style="width: 280rpx;">一行显示不全,wrap折行</view>
+			<view class="text" style="width: 280rpx;">一行显示不全,wrap折行</view>
+		</view> -->
+
+	</view>
+</template>
+
+
+<script>
+	import http from '../../common/request';
+	import navBar from "../../components/navBar";
+	import mixChart from '@/components/mixChart.vue'
+	export default {
+		components: {
+			navBar,
+			mixChart
+		},
+		data() {
+			return {
+				triggered: true,
+				navBar: {
+					bgcolor: '#F8F8F8', //导航背景颜色，不传默认#F8F8F8
+					back: true, //回退箭头，不传默认true
+					backcolor: '#333', //回退箭头颜色，不传默认#333
+					previousText: '', //回退提示，不传默认直接回退
+					title: "工时详情", //本页标题，必传
+					titlecolor: '#333', //本页标题颜色，不传默认#333
+				},
+				mixChartData: [{
+					"totalIdlingTime": 296394,
+					"date": "2023-07-03",
+					"avgRunningTime": 3195,
+					"totalWorkTime": 1571275,
+					"notWorkCars": 9962,
+					"totalDrivingTime": 1105513,
+					"avgWorkTime": 29645,
+					"totalRunningTime": 169368,
+					"workCars": 38,
+					"avgDrivingTime": 20858,
+					"avgIdlingTime": 5592
+				}, {
+					"totalIdlingTime": 0,
+					"date": "2023-07-04",
+					"avgRunningTime": 0,
+					"totalWorkTime": 0,
+					"notWorkCars": 10000,
+					"totalDrivingTime": 0,
+					"avgWorkTime": 0,
+					"totalRunningTime": 0,
+					"workCars": 0,
+					"avgDrivingTime": 0,
+					"avgIdlingTime": 0
+				}, {
+					"totalIdlingTime": 0,
+					"date": "2023-07-05",
+					"avgRunningTime": 0,
+					"totalWorkTime": 0,
+					"notWorkCars": 10000,
+					"totalDrivingTime": 0,
+					"avgWorkTime": 0,
+					"totalRunningTime": 0,
+					"workCars": 0,
+					"avgDrivingTime": 0,
+					"avgIdlingTime": 0
+				}, {
+					"totalIdlingTime": 0,
+					"date": "2023-07-06",
+					"avgRunningTime": 0,
+					"totalWorkTime": 0,
+					"notWorkCars": 10000,
+					"totalDrivingTime": 0,
+					"avgWorkTime": 0,
+					"totalRunningTime": 0,
+					"workCars": 0,
+					"avgDrivingTime": 0,
+					"avgIdlingTime": 0
+				}, {
+					"totalIdlingTime": 0,
+					"date": "2023-07-07",
+					"avgRunningTime": 0,
+					"totalWorkTime": 0,
+					"notWorkCars": 10000,
+					"totalDrivingTime": 0,
+					"avgWorkTime": 0,
+					"totalRunningTime": 0,
+					"workCars": 0,
+					"avgDrivingTime": 0,
+					"avgIdlingTime": 0
+				}, {
+					"totalIdlingTime": 0,
+					"date": "2023-07-08",
+					"avgRunningTime": 0,
+					"totalWorkTime": 0,
+					"notWorkCars": 10000,
+					"totalDrivingTime": 0,
+					"avgWorkTime": 0,
+					"totalRunningTime": 0,
+					"workCars": 0,
+					"avgDrivingTime": 0,
+					"avgIdlingTime": 0
+				}, {
+					"totalIdlingTime": 0,
+					"date": "2023-07-09",
+					"avgRunningTime": 0,
+					"totalWorkTime": 0,
+					"notWorkCars": 10000,
+					"totalDrivingTime": 0,
+					"avgWorkTime": 0,
+					"totalRunningTime": 0,
+					"workCars": 0,
+					"avgDrivingTime": 0,
+					"avgIdlingTime": 0
+				}]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+
+		}
+	}
+</script>
+
+
+<style scoped lang="scss">
+	.content {
+		background: #F0F2F5;
+		min-height: calc(100vh - 0rpx);
+		box-sizing: border-box;
+	}
+
+	.date-select {
+		height: 112rpx;
+		line-height: 112rpx;
+		padding: 0 24rpx;
+		background-color: #fff;
+		text-align: right;
+	}
+
+	.title {
+		height: 96rpx;
+		line-height: 96rpx;
+		font-size: 28rpx;
+		padding-left: 24rpx;
+	}
+
+	.card-box {
+		background-color: #fff;
+		border-radius: 16rpx;
+		padding: 24rpx 10rpx;
+		box-sizing: border-box;
+		margin: 0 24rpx;
+
+		.item {
+			border-radius: 16rpx;
+			padding: 20rpx 0 20rpx 24rpx;
+			-webkit-flex: 1;
+			flex: 1;
+			margin: 10rpx;
+
+			.t {
+				font-size: 28rpx;
+				margin-bottom: 10rpx;
+			}
+
+			.value {
+				font-size: 36rpx;
+
+				.bit {
+					color: #999;
+					font-size: 28rpx;
+					margin-left: 10rpx;
+				}
+			}
+
+		}
+
+		.left-icon {
+			.img {
+				width: 80rpx;
+				height: 80rpx;
+				margin-right: 16rpx;
+				margin-top: 6rpx;
+			}
+		}
+
+		.day-row {
+			border-bottom: 1px solid #E4E5E8;
+			padding-bottom: 50rpx;
+			margin-bottom: 34rpx;
+		}
+
+		.day-row:last-child {
+			border: none;
+			margin-bottom: 0rpx;
+		}
+
+		.day-detail {
+			.value {
+				height: 56rpx;
+				line-height: 56rpx;
+				-webkit-flex: 1;
+				flex: 1;
+				margin: 0 8rpx;
+				background-color: #F4F7FA;
+				padding-left: 24rpx;
+				font-size: 28rpx;
+
+				.val {
+					margin-left: 10rpx;
+				}
+
+				.img {
+					width: 32rpx;
+					height: 32rpx;
+					vertical-align: sub;
+					margin-right: 14rpx;
+				}
+			}
+		}
+
+		.date {
+			padding-left: 10rpx;
+			margin: 0rpx 0 32rpx 0;
+			font-size: 32rpx;
+		}
+	}
+</style>
